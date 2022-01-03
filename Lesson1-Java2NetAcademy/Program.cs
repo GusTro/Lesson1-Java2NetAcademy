ce071744d79471d7be659ad3f78336fc61e68481
gustavo.troncos <gustavo.troncos@PE-IT001257>
2022-01-02 23:33:12 -0500

    Add project files.

---
+++ Lesson1-Java2NetAcademy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1_Java2NetAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
